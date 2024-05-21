7396b340f7d406b8ca1dc5d6f86cc9b2aee99f4d
gorod0k <gorod_ok_77@mail.ru>
2024-05-21 10:21:01 +0700

    fix socket io declaration

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('http').createServer(handler),
-io = require('socket.io').listen(app),
+io = require('socket.io')(app),
 fs = require("fs"),
 url = require("url"),
 port = process.env.PORT || 5000,
